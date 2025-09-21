7ab4111efb2534436926edb5fde840d6e629b403
varvara <sofam2135@gmail.com>
2025-09-21 15:57:50 +0500

    добавила Month, но там еще гет дей не работает потом в мейне сделаем все по красоте

---
+++ src/Month.java
@@ -0,0 +1,37 @@
+public class Month {
+    private int monthNumber;          // номер месяца (1-12)
+    private int daysInMonth;          // количество дней в месяце
+    private Day[] days;
+
+    private static final int[] DAYS_IN_MONTH = {0, 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
+
+    private static final String[] MONTH_NAMES = {"", "Январь", "Февраль", "Март", "Апрель", "Май", "Июнь", "Июль", "Август", "Сентябрь", "Октябрь", "Ноябрь", "Декабрь"};
+
+    public Month(int monthNumber) {
+        if (monthNumber < 1 || monthNumber > 12) {
+            throw new IllegalArgumentException("Неверный номер месяца: " + monthNumber);
+        }
+
+        this.monthNumber = monthNumber;
+        this.daysInMonth = DAYS_IN_MONTH[monthNumber];
+        this.days = new Day[daysInMonth + 1];
+    }
+
+    public int getMonthNumber() { return monthNumber; }
+    public int getDaysInMonth() { return daysInMonth; }
+    public String getMonthName() {
+        return MONTH_NAMES[monthNumber];
+    }
+    public static String getMonthName(int monthNumber) {
+        if (monthNumber < 1 || monthNumber > 12) {
+            throw new IllegalArgumentException("Неверный номер месяца: " + monthNumber);
+        }
+        return MONTH_NAMES[monthNumber];
+    }
+
+    public void addEvent(int dayNumber, String time, String title, String comm) {
+        Day day = getDay(dayNumber);
+        day.addEvent(time, title, comm);
+    }
+
+}
